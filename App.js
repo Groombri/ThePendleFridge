0bc61b430e068c49b9073a3099f05bdf9cb14229
Groombri <e.groombridge@lancaster.ac.uk>
2025-01-15 14:25:24 +0000

    added portrait screen lock for expo

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import DrawerNav from "./app/components/DrawerNav";
 import SafeViewAndroid from "./app/styles/SafeViewAndroid";
+import * as ScreenOrientation from "expo-screen-orientation";
 
 /**
  * The Pendle fridge app driver. Ensures that all contents are contained within a custom SafeAreaView.
  * @returns the screen navigation handler <DrawerNav> within a navigation container and SafeAreaView.
  */
 export default function App() {
+  ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT_UP);
+
   return (
     <SafeAreaView style={SafeViewAndroid.AndroidSafeArea}>
       <NavigationContainer>
@@ -16,5 +19,3 @@ export default function App() {
     </SafeAreaView>
   );
 }
-
-
