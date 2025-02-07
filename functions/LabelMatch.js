b4ae20051b3bf8a535984ab50306286a4266c5b2
Groombri <e.groombridge@lancaster.ac.uk>
2025-02-07 12:18:50 +0000

    working filter applied to notify foods

---
+++ functions/LabelMatch.js
@@ -0,0 +1,47 @@
+/**
+ * Gets relatedness (0 lowest and 1 highest) of 2 words
+ * see: https://github.com/commonsense/conceptnet5/wiki/API
+ */
+async function getSimilarity(word1, word2) {
+  //converts words to accepted format
+  word1 = word1.toLowerCase();
+  word2 = word2.toLowerCase();
+
+  //ConceptNet API endpoint for relatedness of the words
+  const url = `https://api.conceptnet.io/relatedness?node1=/c/en/${word1}&node2=/c/en/${word2}`;
+
+  //return the relatedness value from the api
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data.value;
+  } catch (error) {
+    return 0.0;
+  }
+}
+
+async function LabelMatch(product, userLabel, threshold) {
+  const promises = [];
+
+  //first try with product name
+  promises.push(getSimilarity(product.name, userLabel));
+
+  //then try with individual words of product name
+  const productName = product.name.split(" ");
+  for (const word of productName) {
+    promises.push(getSimilarity(word, userLabel));
+  }
+
+  //then try with product labels
+  for (const label of product.keywords) {
+    promises.push(getSimilarity(label, userLabel));
+  }
+
+  const results = await Promise.all(promises);
+  const highestRelatedness = Math.max(...results);
+
+  console.log(`Results: ${results}`);
+  return highestRelatedness >= threshold;
+}
+
+module.exports = { LabelMatch };
--- functions/index.js
+++ functions/index.js
@@ -4,43 +4,77 @@ const admin = require("firebase-admin");
 
 //import helper functions
 const { WithinGivenTimes } = require("./TestDateUtils");
+const { LabelMatch } = require("./LabelMatch");
 
 initializeApp();
 
 const db = admin.firestore();
 
+/**
+ * This is a firebase cloud function that listens to the contents of the fridge.
+ * Each time a product is added to the fridge database, server-side filtering is applied
+ * to all users to find out whether to notify them or not based on if the new product details
+ * match their notification settings.
+ */
 exports.notifyUsersOnDonation = onValueCreated(
   {
-    region: "europe-west1", // Set region here
-    ref: "/product/{productId}", // Path in the database
+    region: "europe-west1", //database region
+    ref: "/product/{productId}", //database path to products
   },
+  //on product added to fridge
   async (event) => {
-    const newProduct = event.data.val(); // Get the new product data
+    const newProduct = event.data.val(); //get the data of the product
+
     if (!newProduct) return;
 
+    //get all users
     const users = await db.collection("users").get();
 
     const filteredUsers = [];
 
-    users.forEach((doc) => {
+    //for every user
+    for (const doc of users.docs) {
       const user = doc.data();
 
       //don't notify users who have notifications disabled
       if (!user.notificationsEnabled) {
-        return;
+        continue;
       }
+
       //don't notify users where current time doesnt fall between their set notification times
       if (
         user.notificationTimesEnabled &&
         !WithinGivenTimes(user.notificationTimes)
       ) {
-        return;
+        continue;
+      }
+
+      //don't notify users who want specific items that don't match the new item
+      if (user.notifyFoodsEnabled) {
+        if (user.notifyFoods.length < 1) {
+          continue;
+        }
+
+        let matchedItem = false;
+
+        //gets semantic similarity between users interested items and new product
+        for (const food of user.notifyFoods) {
+          console.log(`Food: ${food}, text: ${food.text}`);
+          const isMatch = await LabelMatch(newProduct, food.text, 0.6);
+          if (isMatch) {
+            matchedItem = true;
+          }
+        }
+
+        //if none of the users interested items are deemed similair to the product, don't notify them
+        if (!matchedItem) {
+          continue;
+        }
       }
 
       //if user has passed all filters, add them to the list of users to be notified
       filteredUsers.push(user);
-    });
-
+    }
     console.log("FILTERED USERS:", filteredUsers);
   }
 );
