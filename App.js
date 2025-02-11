628f45ba7827808b834a59b7c538986187dc5e0a
Groombri <e.groombridge@lancaster.ac.uk>
2025-02-11 10:21:32 +0000

    added basic push notifications for android

--- App.js
+++ App.js
@@ -4,18 +4,54 @@ import DrawerNav from "./app/components/DrawerNav";
 import SafeViewAndroid from "./app/styles/SafeViewAndroid";
 import * as ScreenOrientation from "expo-screen-orientation";
 import { StatusBar } from "expo-status-bar";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import AddNewUser from "./app/utils/AddNewUser";
+import * as Notifications from "expo-notifications";
 
 /**
  * The Pendle fridge app driver. Ensures that all contents are contained within a custom SafeAreaView.
  * @returns the screen navigation handler <DrawerNav> within a navigation container and SafeAreaView.
  */
 export default function App() {
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
   useEffect(() => {
-    ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT_UP);
-    AddNewUser();
-  });
+    async function init() {
+      ScreenOrientation.lockAsync(
+        ScreenOrientation.OrientationLock.PORTRAIT_UP
+      );
+      await AddNewUser();
+    }
+
+    //initialise user
+    init();
+
+    Notifications.setNotificationHandler({
+      handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: true,
+        shouldSetBadge: false,
+      }),
+    });
+
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        console.log("Notification Received:", notification);
+      });
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log("User Tapped Notification:", response);
+      });
+
+    return () => {
+      Notifications.removeNotificationSubscription(
+        notificationListener.current
+      );
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
 
   return (
     <View style={{ flex: 1, backgroundColor: "transparent" }}>
